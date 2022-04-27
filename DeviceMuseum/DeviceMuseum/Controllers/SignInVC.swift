c14066f2469abad7306bd9f15dd2da4ed09f3422
DMITRY SMIRNOV <smirnoff145@gmail.com>
2022-04-27 20:02:24 +0300

    project changed #3

--- DeviceMuseum/DeviceMuseum/Controllers/SignInVC.swift
+++ DeviceMuseum/DeviceMuseum/Controllers/SignInVC.swift
@@ -15,6 +15,8 @@ final class SignInVC: UIViewController {
     @IBOutlet private var userErrorLbl: UILabel!
     @IBOutlet weak var loginBtn: UIButton!
     
+    let swiftUIController = UIHostingController(rootView: ContentView())
+    
     override func viewDidLoad() {
             super.viewDidLoad()
         hideKeyboardWhenTappedAround()
@@ -23,12 +25,30 @@ final class SignInVC: UIViewController {
         loginBtn.layer.shadowOffset = CGSize(width: 4.0, height: 4.0)
         }
     
-    @IBAction private func signInTapped() {
-//        let contentView = ContentView()
-//                let hostingController = UIHostingController(rootView: contentView)
-//                if let window = self.view.window {
-//                    window.rootViewController = hostingController
-//                }
+//    @IBAction private func signInTapped() {
+////        let contentView = ContentView()
+////                let hostingController = UIHostingController(rootView: contentView)
+////                if let window = self.view.window {
+////                    window.rootViewController = hostingController
+////                }
+//        guard let email = emailTF.text,
+//            let pass = passTF.text,
+//            checkUser(email: email, pass: pass)
+//            else {
+//            userErrorLbl.isHidden = false
+//            return
+//        }
+////        let contentView = ContentView()
+////        let hostingController = UIHostingController(rootView: contentView)
+////        if let window = self.view.window {
+////            window.rootViewController = hostingController
+////        }
+//
+//        //performSegue(withIdentifier: "ContentView", sender: nil)
+//        //performSegue(withIdentifier: "goToMain", sender: nil)
+//    }
+    
+    @IBAction func goToContentView() { // _ sender: Any
         guard let email = emailTF.text,
             let pass = passTF.text,
             checkUser(email: email, pass: pass)
@@ -36,30 +56,13 @@ final class SignInVC: UIViewController {
             userErrorLbl.isHidden = false
             return
         }
-        let contentView = ContentView()
-        let hostingController = UIHostingController(rootView: contentView)
-        if let window = self.view.window {
-            window.rootViewController = hostingController
+        //navigationController?.pushViewController(swiftUIController, animated: true)
         }
-
-        //performSegue(withIdentifier: "ContentView", sender: nil)
-        //performSegue(withIdentifier: "goToMain", sender: nil)
-    }
     
-    
-    
-    @IBSegueAction func showUserDetailSegue(_ coder: NSCoder) -> UIViewController? {
+    @IBSegueAction func segueToContentView(_ coder: NSCoder) -> UIViewController? {
         return UIHostingController(coder: coder, rootView: ContentView())
     }
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//            //var swiftUIView: AnyView {
-////                segue.identifier { () -> _ in "ContentView"; return AnyView(ContentView())
-//                guard segue.identifier == "ContentView" else { return } //AnyView(ContentView())
-//                //return AnyView(ContentView())
-//        guard let hostingController = segue.destination as? HostingController else { return }
-//            }
-//////
     private func checkUser(email: String, pass: String) -> Bool {
         let emailSaved = UserDefaults.standard.string(forKey: Constants.email)
         let passSaved = UserDefaults.standard.string(forKey: Constants.pass)
@@ -67,3 +70,12 @@ final class SignInVC: UIViewController {
         return userFound
     }
 }
+
+//override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//            //var swiftUIView: AnyView {
+////                segue.identifier { () -> _ in "ContentView"; return AnyView(ContentView())
+//                guard segue.identifier == "ContentView" else { return } //AnyView(ContentView())
+//                //return AnyView(ContentView())
+//        guard let hostingController = segue.destination as? HostingController else { return }
+//            }
+//////
